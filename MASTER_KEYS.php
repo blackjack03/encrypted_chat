8d481c30e67daad68401317a8c547ff6015f0187
Giacomo Casadei <giacomo.casadei03@gmail.com>
2023-12-05 22:32:44 +0100

    Update Master Keys

--- MASTER_KEYS.php
+++ MASTER_KEYS.php
@@ -1,8 +1,8 @@
 <?php
 
-define("key_word", "T:nord#127.0.0.1#=local&Is_Net=SSL=True");
+define("key_word", "KeyWord");
 
 // Additional Authenticated Data (optional)
-define("authentication_phrase", 'sweet-44_tender-9_hot-juicy_pork_chops');
+define("authentication_phrase", 'AuthenticationPhrase');
 
 ?>
